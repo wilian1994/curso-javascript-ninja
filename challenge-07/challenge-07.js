6176561aa213a38df49575d8873e9e3bf44ca4f1
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-07-13 10:28:21 -0300

    finalizando a aula 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -2,32 +2,37 @@
 Crie um array com 5 items (tipos variados).
 */
 // ?
-
+var array = ['wilian', 1994, {"sobrenome": 'wilian'}, true, ['oliveira']]
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
 // ?
-
+function addItem(valor){
+    array.push(valor)
+    return array
+}
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
 // ?
-
+console.log(addItem(28))
+console.log(addItem('Jessica'))
+console.log(addItem({"sobrenome": "Fernanda"}))
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
 // ?
-
+console.log(`O segundo elemento do segundo array é ${array[6]}`)
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
 // ?
-
+console.log(`O primeiro array tem ${array.length}`)
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
@@ -40,7 +45,11 @@ pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
 // ?
-
+var count = 10;
+while(count <= 20){
+    if(count % 2 === 0) console.log(count)
+    count++
+}
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
